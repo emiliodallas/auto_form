34bb29ea60e150b0e460cfd1b47953ee73a9457e
Emilio Dallastella <emilio@alunos.utfpr.edu.br>
2023-07-31 17:24:36 -0300

    Adding SNS Alarm

--- run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-echo "Today is " `date`
-
-echo -e "\nenter the path to directory:"
-read the_path
-
-echo -e "\n your path has the following archives and folders in it:"
-ls $the_path
